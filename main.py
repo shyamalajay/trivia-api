748e053cdabbc91f542f46429e7c106871d4ab81
Shyamala Jayaraman <jayaraman.shyamala@gmail.com>
2026-01-29 09:14:50 -0800

    Revert "Added async" - remove asyc changes
    This reverts commit 29be118e31f0a0c9e4ca48b8783263bace90626f.

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from airtable_client import get_random_fact
 app = FastAPI(title="Public Trivia API")
 
 @app.get("/fact/random")
-async def random_fact(category: str = Query(None)):
-    fact = await get_random_fact(category)
+def random_fact(category: str = Query(None)):
+    fact = get_random_fact(category)
     if not fact:
         raise HTTPException(status_code=404, detail="Uh oh! No fact found")
     return fact
